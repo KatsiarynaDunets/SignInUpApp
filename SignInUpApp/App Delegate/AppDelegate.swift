a186366b23816f6aeb5ef904c7aa2cb598f90c7d
KatsiarynaDunets <kate.dunets@gmail.com>
2023-08-29 00:37:37 +0200

    Version 1.1

--- SignInUpApp/App Delegate/AppDelegate.swift
+++ SignInUpApp/App Delegate/AppDelegate.swift
@@ -9,9 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,15 +27,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
 import CoreData
 
+
 var persistentContainer: NSPersistentContainer = {
     let container = NSPersistentContainer(name: "MyModel") // Имя должно совпадать с именем вашего .xcdatamodeld файла
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+    container.loadPersistentStores(completionHandler: { _, error in
         if let error = error as NSError? {
             fatalError("Unresolved error \(error), \(error.userInfo)")
         }
@@ -50,11 +46,11 @@ func saveUser(userModel: UserModel) {
     let context = persistentContainer.viewContext
     let userEntity = NSEntityDescription.entity(forEntityName: "User", in: context)!
     let user = NSManagedObject(entity: userEntity, insertInto: context)
-    
+
     user.setValue(userModel.name, forKey: "name")
     user.setValue(userModel.email, forKey: "email")
     user.setValue(userModel.pass, forKey: "pass")
-    
+
     do {
         try context.save()
     } catch let error as NSError {
@@ -66,19 +62,19 @@ func fetchUsers() -> [UserModel] {
     let context = persistentContainer.viewContext
     let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
     var users: [UserModel] = []
-    
+
     do {
         let result = try context.fetch(request)
         for data in result as! [NSManagedObject] {
             let name = data.value(forKey: "name") as? String
             let email = data.value(forKey: "email") as! String
             let pass = data.value(forKey: "pass") as! String
-            
+
             users.append(UserModel(name: name, email: email, pass: pass))
         }
     } catch {
         print("Failed fetching")
     }
-    
+
     return users
 }
